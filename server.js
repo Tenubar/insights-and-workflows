2d2ef5c03de839ed832ef0d88e9acf065e190591
tenubar <solartech4756@gmail.com>
2025-04-01 17:08:52 -0400

    update

--- server.js
+++ server.js
@@ -25,12 +25,22 @@ const secretAccessKey = process.env.VITE_AWS_SECRET_ACCESS_KEY;
    },
  });
 
- app.use(
-  cors({
-    origin: "https://insights-and-workflows.onrender.com", // Replace with your frontend URL
-    credentials: true, // Allow cookies to be sent and received
-  })
-);
+//  app.use(
+//   cors({
+//     origin: "https://insights-and-workflows.onrender.com", // Replace with your frontend URL
+//     credentials: true, // Allow cookies to be sent and received
+//   })
+// );
+
+
+app.use(cors({
+  origin: "*", // Allow requests from any domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  exposedHeaders: "Content-Type,Content-Length",
+  credentials: true, // Set to 'true' only if cookies/authentication are required
+}));
+
 
 app.use(cookieParser());
 app.use(bodyParser.json());
